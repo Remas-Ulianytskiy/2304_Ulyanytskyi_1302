949bdab5674b74e657ad9cf2fcddb34dd8d22855
Remas Orca <remas.orca@gmail.com>
2023-06-08 20:37:31 +0300

    v2

--- 2304_Ulyanytskyi_1302/2304_Ulyanytskyi_1302.cpp
+++ 2304_Ulyanytskyi_1302/2304_Ulyanytskyi_1302.cpp
@@ -8,7 +8,7 @@ int get_user_value() {
     return value;
 }
 
-int get_factirial(int number) {
+long int get_factirial(int number) {
     if (number == 0 || number == 1) {
         return 1;
     }
@@ -19,7 +19,8 @@ int get_factirial(int number) {
 
 int main()
 {
-    int user_value, factorial;
+    int user_value, 
+    long int factorial;
 
     cout << "Enter a number (greater than zero) to find its factorial: ";
 

9882aa1b6df98ab6b362eb24df6c3d7c5539a967
Remas Orca <remas.orca@gmail.com>
2023-06-08 20:04:01 +0300

    Add project files.

---
+++ 2304_Ulyanytskyi_1302/2304_Ulyanytskyi_1302.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int get_user_value() {
+    int value;
+    cin >> value;
+    return value;
+}
+
+int get_factirial(int number) {
+    if (number == 0 || number == 1) {
+        return 1;
+    }
+    else {
+        return number * get_factirial(number - 1);
+    }
+}
+
+int main()
+{
+    int user_value, factorial;
+
+    cout << "Enter a number to find its factorial: ";
+
+    user_value = get_user_value();
+    factorial = get_factirial(user_value);
+
+    cout << "The factorial of " << user_value << " is " << factorial;
+
+    return 0;
+}
